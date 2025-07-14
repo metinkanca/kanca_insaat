f5148884a8b26c285b92249a4e6e15eefaedbb42
Metin Kanca <taiyonokage@gmail.com>
2025-07-14 22:06:35 +0300

    Image Fixes

--- src/Images.tsx
+++ src/Images.tsx
@@ -1,9 +1,9 @@
 
 
 import ImageSlider from "./ImageSlider"
-import kombi from "../images/main_page_slider_images/eca_kombi.png"
-import pelet_kazani from "../images/main_page_slider_images/pelet_kazani.png"
-import kalorifer_sobasi from "../images/main_page_slider_images/kalorifer_sobasi.png"
+import kombi from "/images/main_page_slider_images/eca_kombi.png"
+import pelet_kazani from "/images/main_page_slider_images/pelet_kazani.png"
+import kalorifer_sobasi from "/images/main_page_slider_images/kalorifer_sobasi.png"
 const images = [kombi,pelet_kazani,kalorifer_sobasi]
 
 export default function Images(){
